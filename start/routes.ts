6476ca352498300b3e172223fa4d31d1ef29a656
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-21 11:33:48 -0300

    feat: config routes

--- start/routes.ts
+++ start/routes.ts
@@ -6,11 +6,11 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router
+  .group(() => {
+    router.post('/signup', '#controllers/users_controller.create').use(middleware.user())
+  })
+  .prefix('/users')
