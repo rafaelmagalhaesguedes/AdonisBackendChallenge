776d26eae03cd9f9588d325fd6cb4f4267ecff7a
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-21 11:33:05 -0300

    feat: user middleware created

--- start/kernel.ts
+++ start/kernel.ts
@@ -42,5 +42,6 @@ router.use([
  * the routes or the routes group.
  */
 export const middleware = router.named({
+  user: () => import('#middleware/user_middleware'),
   auth: () => import('#middleware/auth_middleware'),
 })
