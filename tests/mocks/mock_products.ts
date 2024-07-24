e4b50750a25e14517badc29d3f97c0767a70c92c
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-23 21:12:01 -0300

    tests: mock product created

---
+++ tests/mocks/mock_products.ts
@@ -0,0 +1,17 @@
+export const mockProductData = {
+  name: 'New Product',
+  description: 'A new product description',
+  price: 19.99,
+  stock: 100,
+  category: 'Electronics',
+  image: 'https://example.com/product-image.jpg',
+}
+
+export const mockInvalidProductData = {
+  name: 'New Product',
+  description: 'A new product description',
+  price: 'invalid-price',
+  stock: 100,
+  category: 'Electronics',
+  image: 'invalid-url',
+}
