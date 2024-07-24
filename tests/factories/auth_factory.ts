e93d90456d568b8caa12640307e843fc45eb5139
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-23 21:11:26 -0300

    tests: auth token factory created

---
+++ tests/factories/auth_factory.ts
@@ -0,0 +1,11 @@
+/*
+    This file contains the factory functions for the authentication module.
+    The factory functions are used to create the data for the tests.
+ */
+export async function loginAndGetToken(client: any) {
+  const loginResponse = await client.post('/auth/login').json({
+    email: 'user@user.com',
+    password: 'secret_user',
+  })
+  return loginResponse.body().token.token
+}
