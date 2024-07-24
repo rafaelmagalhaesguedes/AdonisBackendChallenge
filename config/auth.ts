893b2f13ba2d8742e720346675febe74c469dd23
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-23 21:20:09 -0300

    fix: erros ESLint

--- config/auth.ts
+++ config/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import { defineConfig } from '@adonisjs/auth'
 import { tokensGuard, tokensUserProvider } from '@adonisjs/auth/access_tokens'
 import type { InferAuthEvents, Authenticators } from '@adonisjs/auth/types'
@@ -16,13 +17,10 @@ const authConfig = defineConfig({
 
 export default authConfig
 
-/**
- * Inferring types from the configured auth
- * guards.
- */
 declare module '@adonisjs/auth/types' {
   interface Authenticators extends InferAuthenticators<typeof authConfig> {}
 }
+
 declare module '@adonisjs/core/types' {
   interface EventsList extends InferAuthEvents<Authenticators> {}
 }
