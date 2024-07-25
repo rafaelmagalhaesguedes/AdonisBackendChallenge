720c51fa531a181a246cc45d1ad3ad2e94634187
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-25 10:43:28 -0300

    feat: customer route group created

--- start/routes.ts
+++ start/routes.ts
@@ -5,6 +5,8 @@ const AuthController = () => import('#controllers/auth_controller')
 
 const ProductsController = () => import('#controllers/products_controller')
 
+const CustomersController = () => import('#controllers/customers_controller')
+
 router
   .group(() => {
     router.post('signup', [AuthController, 'register'])
@@ -15,10 +17,20 @@ router
 
 router
   .group(() => {
-    router.get('/list', [ProductsController, 'index']).use(middleware.auth())
-    router.post('/create', [ProductsController, 'store']).use(middleware.auth())
-    router.get('/details/:id', [ProductsController, 'show']).use(middleware.auth())
-    router.patch('/update/:id', [ProductsController, 'update']).use(middleware.auth())
-    router.delete('/delete/:id', [ProductsController, 'destroy']).use(middleware.auth())
+    router.get('list', [ProductsController, 'index']).use(middleware.auth())
+    router.post('create', [ProductsController, 'store']).use(middleware.auth())
+    router.get('details/:id', [ProductsController, 'show']).use(middleware.auth())
+    router.patch('update/:id', [ProductsController, 'update']).use(middleware.auth())
+    router.delete('delete/:id', [ProductsController, 'destroy']).use(middleware.auth())
   })
   .prefix('products')
+
+router
+  .group(() => {
+    router.get('list', [CustomersController, 'index']).use(middleware.auth())
+    router.post('create', [CustomersController, 'store']).use(middleware.auth())
+    router.get('details/:id', [CustomersController, 'show']).use(middleware.auth())
+    router.patch('update/:id', [CustomersController, 'update']).use(middleware.auth())
+    router.delete('delete/:id', [CustomersController, 'destroy']).use(middleware.auth())
+  })
+  .prefix('customers')
